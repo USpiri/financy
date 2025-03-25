938d50a7ad23ae82faa0efc5593ce9de8ace486e
USpiri <urielspiridione135@gmail.com>
2025-03-25 10:41:59 -0300

    ✨ Feat: handle error util

---
+++ server/src/utils/handleError.ts
@@ -0,0 +1,18 @@
+import { Response } from "express";
+
+interface ErrorHandlerExtra {
+  code?: number;
+  errorRaw?: unknown;
+}
+
+export const handleError = (
+  res: Response,
+  message: string,
+  extra: ErrorHandlerExtra = { code: 500, errorRaw: null },
+) => {
+  const { code, errorRaw } = extra;
+  res.status(code ?? 500).send({
+    error: message,
+    detail: errorRaw || null,
+  });
+};
