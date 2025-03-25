839b4bf91122f5fd5191c45898af5cb0cde00b83
USpiri <urielspiridione135@gmail.com>
2025-03-25 18:14:11 -0300

    🔧 Config: Path alias

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -13,5 +14,8 @@ export default defineConfig({
         secure: false,
       },
     },
+  },
+  resolve: {
+    alias: [{ find: "@", replacement: path.resolve(__dirname, "./src") }],
   },
 });
