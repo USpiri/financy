d77d1f7f059b22617cfbdb4497a94ea622c91bc1
USpiri <urielspiridione135@gmail.com>
2025-03-25 11:19:56 -0300

    🔧 Config: Setup environments

--- server/server.ts
+++ server/server.ts
@@ -1,7 +1,6 @@
 import app from "./src";
+import { PORT } from "./src/config/constants";
 
-const port = 3000;
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+app.listen(PORT, () => {
+  console.log(`Financy app listening on port ${PORT}`);
 });
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,8 +1,11 @@
-import express from "express";
-import cors from "cors";
 import { routes } from "@/routes";
+import cors from "cors";
+import dotenv from "dotenv";
+import express from "express";
+import { resolve } from "path";
 
 const app = express();
+dotenv.config({ path: resolve(__dirname, "./.env") });
 
 app.use(express.json());
 app.use(cors());
