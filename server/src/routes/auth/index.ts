85c64941060b0b65dc1df4d622a1a86cbcce42ac
USpiri <urielspiridione135@gmail.com>
2025-03-25 13:01:26 -0300

    ✨ Feat: register validations

--- server/src/routes/auth/index.ts
+++ server/src/routes/auth/index.ts
@@ -1,9 +1,24 @@
 import { login, register } from "@/controllers/auth";
+import { validator } from "@/middlewares/validator";
 import { Router } from "express";
+import { check } from "express-validator";
 
 const router = Router();
 
 router.post("/login", login);
-router.post("/register", register);
+router.post(
+  "/register",
+  [
+    check("name", "Name is required").notEmpty(),
+    check("lastName", "Name is required").notEmpty(),
+    check("email", "Email is required").isEmail(),
+    check(
+      "password",
+      "Password is required on must have at least 6 characters",
+    ).isLength({ min: 6 }),
+    validator,
+  ],
+  register,
+);
 
 export { router as authRouter };
