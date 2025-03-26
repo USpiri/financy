f600c1fc054e7efd438be80df071d8af2b1017f3
USpiri <urielspiridione135@gmail.com>
2025-03-26 15:10:52 -0300

    ✨ Feat: Separate incomes and expenses for each category

--- server/src/services/summary.ts
+++ server/src/services/summary.ts
@@ -12,21 +12,37 @@ export const getSummary = async (userId: string) => {
         where: { userId, type: "expense" },
       }),
       prisma.transaction.groupBy({
-        by: ["category"],
+        by: ["category", "type"],
         _count: { _all: true },
         _sum: { amount: true },
         where: { userId },
       }),
     ]);
 
+    const categoryMap = categoryStats.reduce(
+      (acc, { category, type, _count, _sum }) => {
+        acc[category] = acc[category] || {
+          category,
+          income: 0,
+          expense: 0,
+          count: 0,
+        };
+
+        acc[category][type] = _sum.amount || 0;
+        acc[category].count += _count._all;
+
+        return acc;
+      },
+      {} as Record<
+        string,
+        { category: string; income: number; expense: number; count: number }
+      >,
+    );
+
     const summary = {
       income: income._sum.amount || 0,
       expense: expense._sum.amount || 0,
-      categoryStats: categoryStats.map((category) => ({
-        category: category.category,
-        count: category._count._all,
-        amount: category._sum.amount || 0,
-      })),
+      categoryStats: Object.values(categoryMap),
     };
 
     return { ok: true, summary };
