683239eb9657def79b01e516210d6834cf563bc6
USpiri <urielspiridione135@gmail.com>
2025-03-26 00:19:46 -0300

    ✨ Feat: Add footer

--- app/src/components/layouts/MainLayout.tsx
+++ app/src/components/layouts/MainLayout.tsx
@@ -26,6 +26,16 @@ export const MainLayout = () => {
       <main className="mx-auto w-full max-w-5xl p-4">
         <Outlet />
       </main>
+      <footer className="py-10 text-center font-mono text-xs font-semibold opacity-50">
+        by{" "}
+        <a
+          href="https://github.com/USpiri"
+          target="_blank"
+          className="hover:underline"
+        >
+          Uspiri
+        </a>
+      </footer>
     </div>
   );
 };
