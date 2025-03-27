6b4d574ecfc91b50f804c95fcf536e2e160373f2
USpiri <urielspiridione135@gmail.com>
2025-03-27 00:42:11 -0300

    🟢 Chore: File names

--- server/server.ts
+++
@@ -1,6 +0,0 @@
-import app from "./src";
-import { PORT } from "./src/config/constants";
-
-app.listen(PORT, () => {
-  console.log(`Financy app listening on port ${PORT}`);
-});
--- server/src/server.ts
+++ server/src/server.ts
@@ -3,6 +3,7 @@ import cors from "cors";
 import dotenv from "dotenv";
 import express from "express";
 import { resolve } from "path";
+import { PORT } from "./config/constants";
 
 const app = express();
 dotenv.config({ path: resolve(__dirname, "./.env") });
@@ -18,4 +19,7 @@ app.get("/api/ping", (_req, res) => {
   res.send({ timestamp, uptime });
 });
 
+app.listen(PORT, () => {
+  console.log(`Financy app listening on port ${PORT}`);
+});
 export default app;
